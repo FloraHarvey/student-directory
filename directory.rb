6d674dbc3cd0902b73808886755f457e54568523
Flora Harvey <fharvey555@gmail.com>
2017-01-30 19:25:39 +0000

    Updated to print only names beginning with H

--- directory.rb
+++ directory.rb
@@ -7,7 +7,9 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
+    if student[:name][0] == "H"
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
